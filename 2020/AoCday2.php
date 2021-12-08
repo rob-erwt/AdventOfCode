6989653cf5b168f54181eba2f381159a9d4b65cf
Robert Bouma <info@robertbouma.nl>
2021-12-08 19:51:43 +0100

    AoC 2020, day 2, part 2

--- 2020/AoCday2.php
+++ 2020/AoCday2.php
@@ -6,6 +6,7 @@
 $inputData = explode("\n", file_get_contents('inputday2.txt'));
 
 $nrValidPasswordsPart1 = 0;
+$nrValidPasswordsPart2 = 0;
 
 foreach($inputData as $line) {
     list($count, $char, $password) = explode(" ", $line);
@@ -20,9 +21,14 @@ foreach($inputData as $line) {
     }
 
     // Part 2
-    
+    $passwordArray = str_split($password);
+    // -1 because postions start at 1, not 0
+    if($passwordArray[$min-1] == $char xor $passwordArray[$max-1] == $char) {
+        $nrValidPasswordsPart2++;
+    }
 }
 
 print_r("Nr valid passwords (part 1): " . $nrValidPasswordsPart1 . PHP_EOL);
+print_r("Nr valid passwords (part 2): " . $nrValidPasswordsPart2 . PHP_EOL);
 
 ?>
