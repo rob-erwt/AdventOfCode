a4067d5d22b50bd1419b4642b85be96c0a9c0e2d
Robert Bouma <info@robertbouma.nl>
2021-12-08 13:17:48 +0100

    Day 8, part 2

--- AoCday8.php
+++ AoCday8.php
@@ -31,29 +31,128 @@ foreach($inputData as $line) {
 }
 
 // Part 1
-$countNumbers = array("1" => 0, "4" => 0, "7" => 0, "8" => 0);
+$countNumbers = countNumbers($parsedInput, $digits);
+print_r("Number of 1, 4, 7 & 8 digits: " . array_sum($countNumbers) . PHP_EOL);
 
-foreach($parsedInput as $line) {
-    $digitalOutput = $line['digitalOutput'];
+// Part 2
+$total = 0;
 
-    foreach($digitalOutput as $item) {
-        switch (strlen($item)) {
-            case strlen($digits[1]):
-                $countNumbers[1]++;
-                break;
-            case strlen($digits[4]):
-                $countNumbers[4]++;
-                break;
-            case strlen($digits[7]):
-                $countNumbers[7]++;
-                break;
-            case strlen($digits[8]):
-                $countNumbers[8]++;
-                break;
-        }
+foreach($parsedInput as $item) {
+    $translatedDigits = translateChars($item["signalPattern"], $digits);
+    $translatedDigits = orderTranslatedDigits($translatedDigits);
+    
+    $digitalOutputSorted = orderTranslatedDigits($item['digitalOutput']);
+    
+    $number = '';
+    foreach($digitalOutputSorted as $digitalOutput)
+    {
+        $number .= array_search($digitalOutput, $translatedDigits);
     }
+
+    $total += (int)$number;
 }
 
-print_r("Number of 1, 4, 7 & 8 digits: " . array_sum($countNumbers) . PHP_EOL);
+print_r("Total output values: " . $total . PHP_EOL);
+
+
+function countNumbers($parsedInput, $digits) {
+
+    $countNumbers = array("1" => 0, "4" => 0, "7" => 0, "8" => 0);
+
+    foreach($parsedInput as $line) {
+        $digitalOutput = $line['digitalOutput'];
+
+        foreach($digitalOutput as $item) {
+            switch (strlen($item)) {
+                case strlen($digits[1]):
+                    $countNumbers[1]++;
+                    break;
+                case strlen($digits[4]):
+                    $countNumbers[4]++;
+                    break;
+                case strlen($digits[7]):
+                    $countNumbers[7]++;
+                    break;
+                case strlen($digits[8]):
+                    $countNumbers[8]++;
+                    break;
+            }
+        }
+    }
+
+    return $countNumbers;
+}
+
+function translateChars($signalPattern, $digits)
+{
+    $translatedDigits = array();
+
+    foreach($signalPattern as $item) {
+
+        // 1
+        if(strlen($item) == strlen($digits[1])) {
+            $translatedDigits[1] = $item;
+        }
+
+        // 4
+        if(strlen($item) == strlen($digits[4])) {
+            $translatedDigits[4] = $item;
+        }
+
+        // 7
+        if(strlen($item) == strlen($digits[7])) {
+            $translatedDigits[7] = $item;
+        }
+
+        // 8
+        if(strlen($item) == strlen($digits[8])) {
+            $translatedDigits[8] = $item;
+        }
+    }
+
+    foreach($signalPattern as $item) {
+
+        // 9, 0, or 6
+        if(strlen($item) == 6) {
+            if(sizeof(array_intersect(str_split($item), str_split($translatedDigits[4]))) == 4) {
+                $translatedDigits[9] = $item;
+            } elseif(sizeof(array_intersect(str_split($item), str_split($translatedDigits[1]))) == 2) {
+                $translatedDigits[0] = $item;
+            } else {
+                $translatedDigits[6] = $item;
+            }
+        }
+    }
+
+    foreach($signalPattern as $item) {
+        
+        // 3, 2 or 5
+        if(strlen($item) == 5) {
+            if(sizeof(array_intersect(str_split($item), str_split($translatedDigits[1]))) == 2) {
+                $translatedDigits[3] = $item;
+            } elseif(sizeof(array_intersect(str_split($item), str_split($translatedDigits[9]))) == 4) {
+                $translatedDigits[2] = $item;
+            } else {
+                $translatedDigits[5] = $item;
+            }
+        }
+    }
+
+    return $translatedDigits;
+}
+
+function orderTranslatedDigits($translatedDigits) {
+
+    $sortedDigits = array();
+
+    foreach($translatedDigits as $key => $digit) {
+        $digitArray = str_split($digit);
+        asort($digitArray);
+
+        $sortedDigits[$key] = implode('', $digitArray);
+    }
+    
+    return $sortedDigits;
+}
 
 ?>
