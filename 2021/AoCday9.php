3b762264dd5e3fc49aa55d202b6733f5468ec024
Robert Bouma <info@robertbouma.nl>
2021-12-10 09:55:12 +0100

    AoC 2021, day 9, part 2 incomplete

--- 2021/AoCday9.php
+++ 2021/AoCday9.php
@@ -3,8 +3,8 @@
 /* Code belonging with https://adventofcode.com/2021/day/9 */
 
 // Reading input file
-$inputData = explode("\n", file_get_contents('inputday9.txt'));
-$map = $lowpoints = array();
+$inputData = explode("\n", file_get_contents('inputday9test.txt'));
+$map = $lowpoints = $lowpointBassins = $bassins = array();
 $sumRiskLevels = 0;
 
 $maxWidth = strlen($inputData[0]);
@@ -14,10 +14,12 @@ foreach($inputData as $line) {
     $map[] = str_split($line);
 }
 
+// Part 1
 foreach($map as $lineKey => $line) {
     foreach($line as $posKey => $position) {
         if(isLowPoint($position, $posKey, $lineKey, $map, $maxWidth, $maxHeight)) {
             $lowpoints[] = $position;
+            $lowpointBassins[] = array($lineKey, $posKey);
         }
     }
 }
@@ -27,6 +29,14 @@ foreach($lowpoints as $point) {
 }
 
 print_r("Sum risk levels: " . $sumRiskLevels . PHP_EOL);
+
+// Part 2
+foreach($lowpointBassins as $lowpoint)
+{
+    $bassins[] = getBassinPoints($lowpoint, $map, $maxWidth, $maxHeight);
+}
+
+var_dump($bassins);
 
 function isLowPoint($position, $posKey, $lineKey, $map, $maxWidth, $maxHeight) {
 
@@ -40,6 +50,53 @@ function isLowPoint($position, $posKey, $lineKey, $map, $maxWidth, $maxHeight) {
     if($lineKey + 1 < $maxHeight && $map[$lineKey+1][$posKey] <= $position) { return false; }
 
     return true;
- }
+}
+
+function getBassinPoints($point, $map, $maxWidth, $maxHeight) {
+
+    $neighbours = $newNeighbours = $newPoints = array();
+
+    $bassinPoints[] = $point;
+    $curValue = $map[$point[0]][$point[1]];
+    
+    $neighbours = getNeighbours($point[0], $point[1], $maxWidth, $maxHeight);
+
+    if(sizeof($neighbours)) {
+        foreach($neighbours as $neighbourPoint) {
+            if(
+                $map[$neighbourPoint[0]][$neighbourPoint[1]] < 9 &&
+                $map[$neighbourPoint[0]][$neighbourPoint[1]] >= $curValue
+            ) {
+                $newPoints[] = $neighbourPoint;
+                $bassinPoints[] = $neighbourPoint;
+            }
+        }
+
+        if(sizeof($newPoints)) {
+            foreach($newPoints as $newPoint) {
+                $newNeighbours = getBassinPoints($newPoint, $map, $maxWidth, $maxHeight);
+            }
+        }
+    }
+
+    return array_merge($bassinPoints, $newNeighbours);
+}
+
+function getNeighbours($posKey, $lineKey, $maxWidth, $maxHeight) {
+    
+    if($posKey - 1 >= 0 && $posKey + 1 < $maxWidth && $lineKey - 1 >= 0 && $lineKey + 1 < $maxHeight) {
+    
+        $neighbours = array(
+                array(($lineKey - 1), $posKey),
+                array(($lineKey + 1), $posKey),
+                array($lineKey, ($posKey - 1)),
+                array($lineKey, ($posKey + 1))
+            );
+        
+        return $neighbours;
+    }
+
+    return array();
+}
 
 ?>
