7b74ac614f37b035a902451e3b25ed846b46064e
Robert Bouma <info@robertbouma.nl>
2021-12-14 09:08:45 +0100

    Get rid of the warnings

--- 2021/AoCday13.php
+++ 2021/AoCday13.php
@@ -1,7 +1,6 @@
 <?php
 
 /* Code belonging with https://adventofcode.com/2021/day/13 */
-error_reporting(E_ERROR | E_PARSE);
 
 // Reading input file
 $inputData = explode("\n\n", file_get_contents('inputday13.txt'));
@@ -41,7 +40,7 @@ function printPaperSheet($map) {
     // Print grid
     for($i = 0; $i <= $maxY; $i++) {
         for($j = 0; $j <= $maxX; $j++) {
-            print_r($mapPoints[$i][$j] ?: ".");
+            print_r($mapPoints[$i][$j] ?? ".");
         }
         print_r(PHP_EOL);
     }
