4e7e7bd0fee0fa317559bd3eabee975afcb87fab
Robert Bouma <info@robertbouma.nl>
2022-02-18 16:15:00 +0100

    AoC 2020, day 3, part 1 & 2

--- 2020/AoCday3.php
+++ 2020/AoCday3.php
@@ -5,26 +5,42 @@
 // Reading input file
 $map = explode("\n", file_get_contents('inputday3.txt'));
 
-$lineLength = strlen($map[0]);
-$nrTrees = $nrSquares = $idx = 0;
-$curPos = 0;
+// Part 1
+$nrTrees = traverseMap($map, 3, 1);
 
-foreach($map as $line) {
-    if($idx) {
-        $curPos += 3;
+print_r("PART 1: Nr of trees: " . $nrTrees . PHP_EOL);
 
-        if($curPos > $lineLength) {
-            $curPos = $curPos - $lineLength;
+// Part 2
+$nrTrees = array();
+$slopes = array([1,1], [3,1], [5,1], [7,1], [1,2]);
+
+foreach($slopes as $slope) {
+    $nrTrees[] = traverseMap($map, $slope[0], $slope[1]);
+}
+
+print_r("PART 2: Nr of trees: " . array_product($nrTrees) . PHP_EOL);
+
+
+function traverseMap($map, $posSteps, $lineSteps) {
+
+    $curPos = $nrTrees = 0;
+    $curLine = '';
+
+    for($i = $lineSteps; $i < count($map); $i += $lineSteps) {
+        $curLine = $map[$i];
+
+        $curPos += $posSteps;
+
+        if($curPos > strlen($curLine) - 1) {
+            $curPos = $curPos - strlen($curLine);
         }
 
-        if(substr($line, $curPos, 1) == "#") {
+        if(substr($curLine,$curPos,1) == '#') {
             $nrTrees++;
         }
     }
-
-    $idx++;
+    
+    return $nrTrees;
 }
-
-print_r("Nr of trees: " . $nrTrees . PHP_EOL);
 
 ?>
