73747ddd0735684cae0ef71732d0863827951d46
Robert Bouma <info@robertbouma.nl>
2023-12-01 13:50:23 +0100

    AoC day 1, part 2

--- 2023/AoCday1.php
+++ 2023/AoCday1.php
@@ -1,10 +1,11 @@
 <?php
 
-$calibrationSum = 0;
+$calibrationSumPt1 = $calibrationSumPt2 = 0;
 $inputData = explode("\n", file_get_contents('inputday1.txt'));
 
 foreach($inputData as $line) {
 
+    // PART 1
     $filteredNumbers = array();
 
     $chars = str_split($line);
@@ -15,11 +16,30 @@ foreach($inputData as $line) {
     }
     
     reset($filteredNumbers);
-    $calibrationSum += current($filteredNumbers).end($filteredNumbers);
+    $calibrationSumPt1 += current($filteredNumbers).end($filteredNumbers);
+
+    // PART 2
+    $parsedLine = str_replace(
+                    array("one", "two", "three", "four", "five", "six", "seven", "eight", "nine"),
+                    array("one1one", "two2two", "three3three", "four4four", "five5five", "six6six", "seven7seven", "eight8eight", "nine9nine"),
+                    $line);
+
+    $filteredNumbers = array();
+
+    $chars = str_split($parsedLine);
+    foreach($chars as $char) {
+        if(is_numeric($char)){
+            $filteredNumbers[] = $char;
+        }
+    }
+    
+    reset($filteredNumbers);
+    $calibrationSumPt2 += current($filteredNumbers).end($filteredNumbers);
+
 }
 
-echo "PART 1: Sum all calibrations: " . $calibrationSum . PHP_EOL;
+echo "PART 1: Sum all calibrations: " . $calibrationSumPt1 . PHP_EOL;
 
-//echo "PART 2: Top 3 total: " . array_sum(array_slice($totals, 0, 3)) . PHP_EOL;
+echo "PART 2: Sum all calibrations: " . $calibrationSumPt2 . PHP_EOL;
 
 ?>
