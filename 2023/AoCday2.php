22cff26ed7cb7537876e0a8191e2e141bd3fa442
Robert Bouma <info@robertbouma.nl>
2023-12-02 14:48:20 +0100

    AoC day 2, part 2

--- 2023/AoCday2.php
+++ 2023/AoCday2.php
@@ -1,13 +1,13 @@
 <?php
 
-$gameIdSum = 0;
+$gameIdSum = $gameIdPowerSum = 0;
 $inputData = explode("\n", file_get_contents('inputday2.txt'));
 
 $loaded = array("red" => 12, "green" => 13, "blue" => 14);
 
 foreach($inputData as $line) {
 
-    // PART 1
+    $minNumberList = array("red" => 1, "green" => 1, "blue" => 1);
     list($gameId, $sets) = explode(": ", $line);
 
     $gameId = trim($gameId, 'Game ');
@@ -21,16 +21,23 @@ foreach($inputData as $line) {
         {
             $cube = explode(" ", $cube);
             
+            // PART 1
             if($loaded[$cube[1]] < $cube[0]) {
                 $gameId = 0;
+            }
+
+            // PART 2
+            if($minNumberList[$cube[1]] < $cube[0]) {
+                $minNumberList[$cube[1]] = (int)$cube[0];
             }
         }
     }
 
     $gameIdSum += $gameId;
+    $gameIdPowerSum += ($minNumberList['red'] * $minNumberList['green'] * $minNumberList['blue']);
 }
 
 echo "PART 1: Sum of possible Game ID's: " . $gameIdSum . PHP_EOL;
-//echo "PART 2: Sum all calibrations: " . $calibrationSumPt2 . PHP_EOL;
+echo "PART 2: Sum of powers possible Game ID's: " . $gameIdPowerSum . PHP_EOL;
 
 ?>
