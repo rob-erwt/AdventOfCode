275384ac900da35913bea3dda5c6643c630d2278
Robert Bouma <info@robertbouma.nl>
2023-12-08 14:55:19 +0100

    AoC day 5, part 2 stub

--- 2023/AoCday5.php
+++ 2023/AoCday5.php
@@ -1,28 +1,27 @@
 <?php
 
-$inputData = explode("\n\n", file_get_contents('inputday5test.txt'));
+$inputData = explode("\n\n", file_get_contents('inputday5.txt'));
 
-$lowestLocationNumber = 0;
 $locations = array();
 
 $seeds = explode(" ", trim(array_shift($inputData), "seeds: "));
 
+// Part 1
 foreach($seeds as $seed) {
-    
-    $destination = getDestination($seed, createMapping($inputData[0]));
-    $destination = getDestination($destination, createMapping($inputData[1]));
-    $destination = getDestination($destination, createMapping($inputData[2]));
-    $destination = getDestination($destination, createMapping($inputData[3]));
-    $destination = getDestination($destination, createMapping($inputData[4]));
-    $destination = getDestination($destination, createMapping($inputData[5]));
-    $destination = getDestination($destination, createMapping($inputData[6]));
 
-    $locations[] = $destination;
+    $locations[] = walkMapping($seed, $inputData);
 }
 
 sort($locations);
 
 echo "PART 1: Lowest location number " . array_shift($locations) . PHP_EOL;
+
+// Part 2
+$location = 0;
+//$location = getSeedsRange($seeds, $inputData);
+
+echo "PART 2: Lowest location number " . $location . PHP_EOL;
+
 
 function createMapping($input) {
 
@@ -53,6 +52,45 @@ function getDestination($source, $mapping) {
     }
 
     return (int)$destination;
+}
+
+function walkMapping($seed, $inputData) {
+
+    $destination = getDestination($seed, createMapping($inputData[0]));
+    $destination = getDestination($destination, createMapping($inputData[1]));
+    $destination = getDestination($destination, createMapping($inputData[2]));
+    $destination = getDestination($destination, createMapping($inputData[3]));
+    $destination = getDestination($destination, createMapping($inputData[4]));
+    $destination = getDestination($destination, createMapping($inputData[5]));
+    $destination = getDestination($destination, createMapping($inputData[6]));
+
+    return $destination;
+}
+
+function getSeedsRange($seeds, $inputData) {
+    
+    $location = 0;
+
+    for($idx = 0; $idx < $seeds[1]; $idx++) {
+        
+        $destination = walkMapping($seeds[0] + $idx, $inputData);
+        echo $destination . PHP_EOL;
+
+        if($location == 0) { $location = $destination; } 
+        
+        if($destination < $location) { $location = $destination; }
+    }
+
+    //for($idx = 0; $idx < $seeds[3]; $idx++) {
+        
+   //     $destination = walkMapping($seeds[2] + $idx, $inputData);
+
+   //     if($location == 0) { $location = $destination; } 
+        
+  //      if($destination < $location) { $location = $destination; }
+   // }
+
+    return $location;
 }
 
 ?>
