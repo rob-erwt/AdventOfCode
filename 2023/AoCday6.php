76630afcf9f3b03ed351666153e1833ebe64cb23
Robert Bouma <info@robertbouma.nl>
2023-12-08 15:31:54 +0100

    AoC day 6, part 2

--- 2023/AoCday6.php
+++ 2023/AoCday6.php
@@ -2,6 +2,7 @@
 
 $inputData = explode("\n", file_get_contents('inputday6.txt'));
 
+// Part 1
 $time = preg_split("/[\s]+/", trim($inputData[0], "Time:"), -1, PREG_SPLIT_NO_EMPTY);
 $distance = preg_split("/[\s]+/", trim($inputData[1], "Distance:"), -1, PREG_SPLIT_NO_EMPTY);
 $races = array_combine($time, $distance);
@@ -13,6 +14,14 @@ foreach($races as $raceLength => $recordDistance) {
 }
 
 echo "PART 1: Multiply winning numbers " . array_product($winningRaces) . PHP_EOL;
+
+// Part 2
+$time = str_replace(" ", "", trim($inputData[0], "Time:"));
+$distance = str_replace(" ", "", trim($inputData[1], "Distance:"));
+
+$winningRaces = calculateNrWinningGames($time, $distance);
+
+echo "PART 2: Multiply winning numbers " . $winningRaces . PHP_EOL;
 
 function calculateNrWinningGames($raceLength, $recordDistance) {
 
