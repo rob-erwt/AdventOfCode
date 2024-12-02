644eae1375c4950d9434007a8aa5e972ffb8616c
Robert Bouma <info@robertbouma.nl>
2024-12-02 11:53:51 +0100

    AoC 2024, day 1, part 1 & 2

--- 2024/AoCday1.php
+++ 2024/AoCday1.php
@@ -1,5 +1,32 @@
 <?php
 
-echo 'test\n':
+$inputData = explode("\n", file_get_contents('inputday1.txt'));
+
+foreach($inputData as $line) {
+    $line = explode("   ", $line);
+    $locationsLeft[] = $locationsLeft2[] = $line[0];
+    $locationsRight[] = $locationsRight2[]= $line[1];
+}
+
+sort($locationsLeft);
+sort($locationsRight);
+
+$totalDistance = $similarityScore = 0;
+
+for($idx = 0; $idx < sizeof($locationsLeft); $idx++) {
+    $totalDistance += abs($locationsLeft[$idx] - $locationsRight[$idx]);
+}
+
+echo "PART 1: Total distance " . $totalDistance . PHP_EOL;
+
+$nrOcurrences = array_count_values($locationsRight2);
+
+foreach($locationsLeft2 as $locationNr) { 
+    if(isset($nrOcurrences[$locationNr])) {
+        $similarityScore += ($nrOcurrences[$locationNr] * $locationNr);
+    }
+}
+
+echo "PART 2: Similarity score " . $similarityScore . PHP_EOL;
 
 ?>
